09f7813c782b41011fce7be7746744fffcceb094
lcfernandez <lcfernandez@cos.ufrj.br>
2023-02-09 09:57:06 -0300

    fix: adjust jest config

--- jest.config.js
+++ jest.config.js
@@ -5,5 +5,6 @@ module.exports = {
   transform: {
     ".+\\.ts$": "ts-jest",
   },
-  testMatch: ["<rootDir>/tests/*.(test|spec).ts"],
+  testMatch: ["<rootDir>/tests/**/*.(test|spec).ts"],
+  setupFiles: ["<rootDir>/src/config/envs.ts"]
 };
